f1b1c73527222c75f71238020daa386a380caa1f
Realdiamond <akinsanmioluwatimilehin@gmail.com>
2026-05-10 22:51:47 +0100

    refactor: rename folders to kebab-case and update imports

--- src/app/(main)/(landing-routes)/(home)/page.tsx
+++ src/app/(main)/(landing-routes)/(home)/page.tsx
@@ -1,9 +1,9 @@
-import Features from '~/features/landing/components/featureSection/features'
+import Features from '~/features/landing/components/feature-section/features'
 import { Hero } from '~/features/landing/components/hero/Hero'
 import HowItWorks from '~/features/landing/components/how-it-works/HowItWorks'
-import { TrustTransparency } from '~/features/landing/components/trustTransparency/TrustTransparency'
+import { TrustTransparency } from '~/features/landing/components/trust-transparency/TrustTransparency'
 import PricingSection from '~/features/landing/components/pricing-section'
-import WhyChoose from '~/features/landing/components/reason/WhyChoose'
+import WhyChoose from '~/features/landing/components/why-choose/WhyChoose'
 
 export default function Home() {
   return (
